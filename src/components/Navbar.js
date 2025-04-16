32a1218d86c207e90e7dca786a4b668db60af19d
gerarjon <gerarsuaverdez@gmail.com>
2025-04-16 16:57:38 -0700

    added animation to navbar and adjusted padding of h1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
+"use client"
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const isScrollingDown = currentScrollPos > prevScrollPos;
+      setVisible(!isScrollingDown);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [prevScrollPos]);
+
+
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${visible ? "navbar-visible" : "navbar-hidden"}`}>
       <div className="logo-container">
           <Link href="/">
-            <Image 
+            <img 
               src="/assets/logo2.png"
               alt="Logo" 
-              fill
+              fill="true"
               style={{
+                display: "block",
                 objectFit: "cover",
-                height: "100%"
+                height: "100%",
+                width: "100%",
+                objectPosition: "center center",
               }}
             />
           </Link>
