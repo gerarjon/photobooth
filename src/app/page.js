126409e95a25242ba728232540981f9bb4de202d
gerarjon <gerarsuaverdez@gmail.com>
2025-04-22 22:52:10 -0700

    added floating photostrips to hero

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,10 @@
 
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { Lilita_One } from "next/font/google";
+import sample1 from "@/assets/sample1.png";
+import sample2 from "@/assets/sample2.png";
 
 const lilita_one = Lilita_One({
   weight: '400',
@@ -22,6 +25,10 @@ export default function Home() {
             <h1>Share.</h1>
           </div>
 
+          <div className="hero-subhead">
+            <p>Online Photo Booth</p>
+          </div>
+
           <div className="hero-description">
             <p>Create adorable photo strips in just seconds. Add cute  stickers, customize with frames, and export in one click. All online,  all FREE, all fun!</p>
           </div>
@@ -30,7 +37,28 @@ export default function Home() {
             <Link href="/photobooth">Get Started!</Link>
           </button>
         </div>
-        <div className="hero-images">
+
+        <div className="hero-card-container">
+          <div className="hero-card">
+            <Image 
+              src={sample2} 
+              priority
+              alt="" 
+              className="hero-card-image-2"
+              placeholder="blur" 
+              objectFit="cover" 
+            /> 
+          </div>
+          <div className="hero-card">
+            <Image 
+              src={sample1} 
+              priority
+              alt="" 
+              className="hero-card-image-1"
+              placeholder="blur" 
+              objectFit="cover"
+            /> 
+          </div>
 
         </div>
       </section>
